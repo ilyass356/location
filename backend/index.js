979a92eaf3103c542a8ce30b83d8b154d149568c
ilyass356 <taifourilyass0609@gmail.com>
2025-08-25 14:58:33 +0000

    get route to test the API

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,18 @@ const mongoose = require('mongoose');//library to connect with Mongo DB database
 
 const app = express();//creating an express app
 
-mongoose.connect('mongodb://localhost:27017/mylocal')
+mongoose.connect('mongodb://localhost:27017/mylocal')//connect to a local mongoDB database 
 .then(()=>{
     console.log('connected to mongoDB');
 }).catch(err=>{
     console.error('Error connecting to mongoDB:',err);
 });
 
-app.use(express.json());
+app.use(express.json());//middleware allows my app to accept the JSON data in requests
+
+//app.use('/',User);//this tells express to use the router for any req starting with "/"
+
+app.get('/test', (req,res) => {//get route to test the API is working
+    res.send('Welcome to the User API');
+});
+
