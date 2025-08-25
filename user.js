26603077653e94bfb66cb143383d140d2dd9bc4b
ilyass356 <taifourilyass0609@gmail.com>
2025-08-25 16:17:55 +0000

    userSchema

--- user.js
+++ user.js
@@ -1,2 +1,24 @@
 const mongoose = require('mongoose');// connecting to mongodb database
 
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    phone:{
+        type:Number,
+        required:true,
+    },
+    id:{
+        type:Number,
+        required:true,
+    }
+
+});
+
+
