ab2ee2de14cb167cabdc971f94ec676e92c1ed1a
ilyass356 <taifourilyass0609@gmail.com>
2025-08-25 14:35:42 +0000

    creating app

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,5 @@
 const express = require('express');//framework for node.js to create routes and API
 const mongoose = require('mongoose');//library to connect with Mongo DB database
+
+
+const app = express();//creating an express app
