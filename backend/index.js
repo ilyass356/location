0324ab18901fa0817e2cea61b4bf8a99dfa9fe1f
ilyass356 <taifourilyass0609@gmail.com>
2025-08-25 14:32:58 +0000

    library to database

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,2 @@
-const express = require('express');
+const express = require('express');//framework for node.js to create routes and API
+const mongoose = require('mongoose');//library to connect with Mongo DB database
