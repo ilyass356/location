6d8debdd4bdf040405515820f5f36b0ad24aebd3
ilyass356 <taifourilyass0609@gmail.com>
2025-08-27 15:18:28 +0000

    add Prod

--- backend/schema/listing.js
+++ backend/schema/listing.js
@@ -4,5 +4,10 @@ const listSchema = new mongoose.Schema({
     Title:{
         type:String,
         required:true,
-    }
+    },
+    Production:{
+        type:Number,
+        required:true,
+    },
+
 })
