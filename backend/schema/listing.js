2a12c4babb1486db43d84ee6bfc580f3194cd43a
ilyass356 <taifourilyass0609@gmail.com>
2025-08-27 15:19:36 +0000

    recreating new one

--- backend/schema/listing.js
+++ backend/schema/listing.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
 
 const listSchema = new mongoose.Schema({
-    Title:{
-        type:String,
-        required:true,
-    },
-    Production:{
-        type:Number,
-        required:true,
-    },
+
 
 })
