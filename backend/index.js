8da50cd2d54a463ef8917204a60e8a3e19ebdaf3
ilyass356 <taifourilyass0609@gmail.com>
2025-08-29 21:03:41 +0000

    add app listen

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');//framework for node.js to create routes and API
 const mongoose = require('mongoose');//library to connect with Mongo DB database
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 require('dotenv').config();//access to .env
 
@@ -20,5 +20,7 @@ app.use(express.json());//middleware allows my app to accept the JSON data in re
 app.get('/test', (req,res) => {//get route to test the API is working
     res.send('Welcome to the User API');
 });
+
+app.listen(port,() => console.log(`server start on ${port}`))
 
 
