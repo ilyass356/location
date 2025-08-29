08db4b7cde491ee1ad89afb0f7a0651ec107112e
ilyass356 <taifourilyass0609@gmail.com>
2025-08-29 21:00:15 +0000

    add the PORT

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');//framework for node.js to create routes and API
 const mongoose = require('mongoose');//library to connect with Mongo DB database
-
+const PORT = process.env.PORT || 5000
 
 require('dotenv').config();//access to .env
 
