2ea7dbb660a3a18973b4f5e9e1999197a794028d
ilyass356 <taifourilyass0609@gmail.com>
2025-08-30 19:35:10 +0000

    add CRUD in the controllers

--- backend/controllers/firstController.js
+++ backend/controllers/firstController.js
@@ -1,5 +1,30 @@
 const getRoutes = (req , res) => {
     res.json({message:'welcome to my API'})
-}
+};
 
-module.exports = {getRoutes}
+
+const postRoutes = (req , res) => {
+    res.json({message:'welcome to my API'})
+};
+
+
+const putRoutes = (req , res) => {
+    res.json({message:'welcome to my API'})
+};
+
+
+const deleteRoutes = (req , res) => {
+    res.json({message:'welcome to my API'})
+};
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {getRoutes,}
