d8e9befb35684e5cf5986accc96f2ac21b621443
ilyass356 <taifourilyass0609@gmail.com>
2025-08-30 19:04:39 +0000

    change the app.get to another file

--- backend/routes/firstController.js
+++ backend/routes/firstController.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-
+app.get('/test', (req,res) => {//get route to test the API is working
+    res.json({message:'welcome to my API'});
+});
 
 module.exports = router
