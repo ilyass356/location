f5d4100a45d4c9e1e55227b39451c09ed869076c
ilyass356 <taifourilyass0609@gmail.com>
2025-08-30 19:06:35 +0000

    adding an app.use to connect the router with the index

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,7 @@ app.use(express.json());//middleware allows my app to accept the JSON data in re
 
 //app.use('/',User);//this tells express to use the router for any req starting with "/"
 
-app.get('/test', (req,res) => {//get route to test the API is working
-    res.json({message:'welcome to my API'});
-});
+app.use('/',require('./routes/firstController'))
 
 app.listen(port,() => console.log(`server start on ${port}`))//express.js method that starts a server and makes it listen to HTTP req
 
--- backend/routes/firstController.js
+++ backend/routes/firstController.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-app.get('/test', (req,res) => {//get route to test the API is working
+router.get('/test', (req,res) => {//get route to test the API is working
     res.json({message:'welcome to my API'});
 });
 
