2ec82c25f1621bb85eb34426c9104b2b1df68976
ilyass356 <taifourilyass0609@gmail.com>
2025-08-30 19:12:49 +0000

    CRUD

--- backend/routes/firstController.js
+++ backend/routes/firstController.js
@@ -5,16 +5,16 @@ router.get('/', (req,res) => {
     res.json({message:'welcome to my API'});
 });
 
-router.get('/', (req,res) => {
-    res.json({message:'welcome to my API'});
+router.post('/', (req,res) => {
+    res.json({message:'create an API'});
 });
 
-router.get('/', (req,res) => {
-    res.json({message:'welcome to my API'});
+router.put('/:id', (req,res) => {
+    res.json({message:'update an API'});
 });
 
-router.get('/', (req,res) => {
-    res.json({message:'welcome to my API'});
+router.delete('/:id', (req,res) => {
+    res.json({message:'delete an API'});
 });
 
 module.exports = router
